4c012be912f518b15de78e49aa176ec2b92f0702
Saurabh 17. Kumar (EXT-Nokia) <saurabh.17.kumar.ext@nokia.com>
2024-12-24 18:36:48 +0530

    dusra code

--- main.tf
+++ main.tf
@@ -2,3 +2,7 @@ resource "azurerm_resource_group" "etesting1" {
   name     = "testing1"
   location = "West Europe"
 }
+resource "azurerm_resource_group" "testing2" {
+  name     = "testing2"
+  location = "West Europe"
+}
